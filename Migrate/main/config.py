36402295d4df880e3b4492029703acb1ebf5e1b3
vishvajit <vishvajit.rao@in.nuagebiz.tech>
2021-03-23 18:59:50 +0530

    Some new files added

--- Migrate/main/config.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/vishvajit/Desktop/Test/test.db'
-
-db = SQLAlchemy(app)
--- Migrate/main/model.py
+++
@@ -1,6 +0,0 @@
-from main.config import db
-
-class Books(db.Model):
-    __tablename__ = 'Books'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
